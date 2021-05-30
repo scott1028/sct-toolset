9566a525c4e4bd02abbae8a50ed1035f866e787b
scott1028 <mic1028002@gmail.com>
2021-05-30 20:35:21 +0800

    wip;

--- packages/core/lib/core.js
+++ packages/core/lib/core.js
@@ -1,7 +1,27 @@
 'use strict';
 
-module.exports = core;
+import get from 'lodash/get';
 
-function core() {
-    // TODO
+// NOTE: digital-string addtraction/subtraction
+export const core = (...args) => {
+  const { 0: rawX, 1: rawY, ...restRawArgs } = args;
+  if (rawY === undefined)
+    return +rawX;
+  const newRestArgs = Object.entries(restRawArgs).map(({1: v}) => v);
+  const x = [];
+  const y = [];
+  const z = [];
+  let tmp = 0;
+  for(let char of `${rawX}`)
+    x.unshift(+char);
+  for(let char of `${rawY}`)
+    y.unshift(+char);
+  for(let idx = 0; idx < Math.max(x.length, y.length); idx++) {
+    const sub = get(x, idx, 0) + get(y, idx, 0) + tmp;
+    tmp = sub >= 10 ? 1 : 0;
+    z.unshift(sub % 10);
+  }
+  return core(`${tmp}${z.join('')}`, ...newRestArgs);
 }
+
+export default core;
